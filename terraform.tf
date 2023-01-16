8d32b6aa4114b02e32c431bd6e298d0126b944c3
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-16 10:16:06 -0500

    removed AWS reference

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.3.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.49.0"
-    }
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
       version = "0.55.1"
